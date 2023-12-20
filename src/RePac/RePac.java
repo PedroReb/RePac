786c345334354091dbbbeaddf028cbc7fdc50dc7
Administrador <Administrador@2DAM01>
2023-12-20 18:57:22 +0100

    Modificacion en javaDoc

--- src/RePac/RePac.java
+++ src/RePac/RePac.java
@@ -4,6 +4,7 @@ public class RePac {
 /**
  * 
  * @author PedroR
+ * @version 1.0 20/12/2023
  * 
  */
 	public static void main(String[] args) {
@@ -11,7 +12,9 @@ public class RePac {
 		 
 		   //La vista se inicia pero no se muestra (visible = false)
 		   Vista vista = new Vista();
-		 
+		 /**
+		  * @return Devolvera la pantalla de carga y la pantalla inicial
+		  */
 		   //El controlador arranca 
 		   Controlador controlador = new Controlador(vista,modelo, null);
 		 
